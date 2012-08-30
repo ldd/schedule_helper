0589dfa44ae3cde97078d945f8bee8051d8b7a3b
ldd <ldd@ldd-laptop.(none)>
2012-08-30 16:57:32 -0400

    getting list no longer requires login

--- get_list.py
+++ get_list.py
@@ -7,26 +7,21 @@ for the 'subject'/'faculty' in question.
 Saves to 'path'
 '''
 def fetch_list(subject,faculty,path):
-	username = raw_input("Please enter your username(ex:john.smith@mail.mcgill.ca)\n")
-	password = getpass.getpass(
-	"Please enter your password\n[Press enter when done. HIDDEN FOR YOUR PROTECTION]\n")
 
 	try:
 		#login to McGill
 		cj = cookielib.CookieJar()
 		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj))
-		login_data = urllib.urlencode({'sid' : username, 'PIN' : password})
 		opener.open('https://banweb.mcgill.ca/pban1/twbkwbis.P_ValLogin')
-		opener.open('https://banweb.mcgill.ca/pban1/twbkwbis.P_ValLogin', login_data)
 
 		#get the table
 		#note: why is this data so ugly? Pretty methods don't work. *sighs*
-		data = ('term_in=201209&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy' +
+		data = ('display_mode_in=LIST&search_mode_in=&term_in=201209&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy' +
 		'&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj='+subject +
 		'&sel_coll=' + faculty + '&sel_crse=%25&sel_title=%25&sel_schd=%25&sel_from_cred=0&sel_to_cred=99'+
 		'&sel_levl=%25&sel_ptrm=%25&sel_instr=%25&sel_attr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a')
 
-		gpaStuff = opener.open('https://banweb.mcgill.ca/pban1/bwskfcls.P_GetCrse', data)
+		gpaStuff = opener.open('https://banweb.mcgill.ca/pban1/bwckschd.p_get_crse_unsec', data)
 		gpaHtml = gpaStuff.read()
 		
 		#write the table to a file
